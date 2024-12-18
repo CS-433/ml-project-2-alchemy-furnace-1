b7d91a4dc236fb408331f4da9e999ea478370372
wangyifanust <ywangpa@connect.ust.hk>
2024-12-18 15:13:04 +0100

    12.18v2

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from PIL import Image
-import torch
-from pathlib import Path
-from os.path import splitext
-
-file = '/home/yeguo/MLp2/outputs/checkpoint_epoch20_0.7186881899833679/test_1_OUT.png'
-img = Image.open(file)
-print(img.size)
